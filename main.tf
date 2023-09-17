5987be2ebcadf524729f8edf80243b7b79b6a9b5
amit-cloudavengers <145281420+amit-cloudavengers@users.noreply.github.com>
2023-09-17 21:49:22 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,21 @@
+provider "google" {
+  project     = "cmedemo"
+  region      = "us-central1"
+}
+
+resource "google_bigquery_dataset" "dataset" {
+  dataset_id                  = "example_dataset"
+  friendly_name               = "test"
+  description                 = "This is a test description"
+  location                    = "US"
+  default_table_expiration_ms = 3600000
+
+  labels = {
+    env = "default"
+  }
+
+}
+
+resource "google_service_account" "bqowner" {
+  account_id = "bqowner"
+}
